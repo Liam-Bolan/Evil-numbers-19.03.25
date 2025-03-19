dbb51e375446d1f6ce3be3cc93b5bca2ee36a760
Liam Bolan <AQ231688@aquinas.ac.uk>
2025-03-19 10:10:46 +0000

    Add project files.

---
+++ Evil numbers 19.03.25/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evil_numbers_19._03._25
+{
+    internal class Program
+    {
+        int[] binary = new int[] { 512, 256, 128, 64, 32, 16, 8, 4, 2, 1 };
+        static void Main(string[] args)
+        {
+            int num = 1;
+            List<string> decnums = new List<string>();
+            List<string> evilnums = new List<string>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                decnums.Add(num.ToString());
+                num += 1;
+            }
+            
+            
+        }
+    }
+}
